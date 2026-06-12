9b424c7b23535138ee525bccbcfe184e8918c8c5
yancheex2 <yancheex2@Inbox.ru>
2026-06-12 16:47:56 +0500

    troll_spell_ms
    troll_spell_ms
    30 / 35 / 40 --> 15 / 35 / 55

--- game/trollnelves2/scripts/vscripts/modifiers/modifier_troll_spell_ms.lua
+++ game/trollnelves2/scripts/vscripts/modifiers/modifier_troll_spell_ms.lua
@@ -16,10 +16,10 @@ end
 
 function modifier_troll_spell_ms:GetModifierMoveSpeedBonus_Special_Boots()
 	if self:GetStackCount() == 1 then 
-		return 30
+		return 15
 	elseif self:GetStackCount() == 2  then
 		return 35
 	elseif self:GetStackCount() == 3  then
-		return 40
+		return 55
 	else return 0 end
 end
